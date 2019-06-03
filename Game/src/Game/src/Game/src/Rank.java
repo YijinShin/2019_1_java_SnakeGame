7df714564c711dc7cccb0859b761fb0ab443741b
Yijin <49178058+o0heama0o@users.noreply.github.com>
2019-06-03 10:32:06 +0900

    re add

--- Game/src/Game/src/Game/src/Rank.java
+++ Game/src/Game/src/Game/src/Rank.java
@@ -11,39 +11,39 @@ import java.util.Collections;
 
 public class Rank {
 		//the array where the scores are deposited
-		private ArrayList<Score> scores;          
+		private ArrayList<Score> scores;
 		//the name of the file where the rankings will be saved
-		private static final String RANKING_FILE = "rankings.dat"; 
-		
+		private static final String RANKING_FILE = "rankings.dat";
+
 		ObjectOutputStream outputStream = null;
 		ObjectInputStream inputStream = null;
-		
+
 		//Initialize the score
 		public Rank() {
 			scores = new ArrayList<Score>();
 		}
-		
+
 		//Sets the name of Player and his score at one String
 		public String getRankedString() {
 			String rankedString = "";
 			int max = 10;
-			
+
 			ArrayList<Score> scores;
 			scores = getScores();
-			
+
 			int i = 0;
 			int x = scores.size();
 			if (x > max) {
 				x = max;
 			}
 			while (i < x) {
-				rankedString += (i + 1) + ".\t" + scores.get(i).getName() + 
+				rankedString += (i + 1) + ".\t" + scores.get(i).getName() +
 						"\t\t" + scores.get(i).getScore() + "\n";
 				i++;
 			}
 			return rankedString;
 		}
-		
+
 		//gets the score and Player-name, and updates(writes) it to the Ranking file
 		public void updateScorefile() {
 			try {
@@ -56,7 +56,7 @@ public class Rank {
 			catch (IOException e) {
 				System.out.println ("Input or Output Error!!" + e.getMessage());
 			}
-					
+
 			try {
 				if (outputStream != null) {
 					outputStream.flush();
@@ -67,8 +67,8 @@ public class Rank {
 				System.out.println ("Input or Output Error!!" + e.getMessage());
 			}
 		}
-		
-		//from the file it gets the String which shows name and score of the five best Player 
+
+		//from the file it gets the String which shows name and score of the five best Player
 		public void loadScoreFile() {
 			try {
 				inputStream = new ObjectInputStream(new FileInputStream(RANKING_FILE));
@@ -83,7 +83,7 @@ public class Rank {
 			catch (ClassNotFoundException e) {
 				System.out.println ("Class Not Found!!" + e.getMessage());
 			}
-			
+
 			try {
 				if (outputStream != null) {
 					outputStream.flush();
@@ -94,31 +94,23 @@ public class Rank {
 				System.out.println ("Input or Output Error!!" + e.getMessage());
 			}
 		}
-		
+
 		//it takes the score of ranker
 		public ArrayList<Score> getScores() {
 			loadScoreFile();
 			sort();
 			return scores;
 		}
-		
+
 	    //sorting the rank of Players according to their scores
 		private void sort() {
 			RankComparator comparator = new RankComparator();
 			Collections.sort(scores, comparator);
 		}
-		
+
 		public void addScore (String name, int score) {
 			loadScoreFile();
 			scores.add(new Score(name, score));
 			updateScorefile();
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
--- Game/src/Game/src/Game/src/RankComparator.java
+++ Game/src/Game/src/Game/src/RankComparator.java
@@ -7,7 +7,7 @@ public class RankComparator implements Comparator<Score> {
 	public int compare (Score score1, Score score2) {
 		int s1 = score1.getScore();
 		int s2 = score2.getScore();
-			
+
 		if (s1 > s2) {
 			return -1;
 		}
