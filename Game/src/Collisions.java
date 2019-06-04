fe1ea9da3f5e596383dba4adbf06df3ca27a87db
RyeHa <21600015@handong.edu>
2019-06-04 21:38:36 +0900

    랭킹 완료 (변수 수정)

--- Game/src/Collisions.java
+++ Game/src/Collisions.java
@@ -13,6 +13,10 @@ public class Collisions {
     private ArrayList<Poison> poison = new ArrayList<Poison>();
     private boolean fstPlayerWin = false;
     private boolean sndPlayerWin = false;
+    
+    private int distance = 10;
+    private int nmScore = 100;
+    private int fvScore = 1;
 
     // Constructor�쓣 留뚮뱾�븣 snake1, snake2, food, poison 媛앹껜 reference瑜� �쟾�떖�빐以�
     public Collisions(Snake snake1, Snake snake2, Food food, ArrayList<Poison> poison) {
@@ -29,11 +33,11 @@ public class Collisions {
 	    	for(int i = 0; i < heightNum; i++) {
 	            for(int j = 0; j < widthNum; j++) {
 
-	            	if ((proximity(snake1.getSnakeX(0), j * PIXELSIZE, 11))
-	                        && (proximity(snake1.getSnakeY(0), i * PIXELSIZE, 11))) {
+	            	if ((proximity(snake1.getSnakeX(0), j * PIXELSIZE, distance + 1))
+	                        && (proximity(snake1.getSnakeY(0), i * PIXELSIZE, distance + 1))) {
 	            		count++;
 	            		// Add score
-	                    snake1.setScore(snake1.getScore() + 1);
+	                    snake1.setScore(snake1.getScore() + fvScore);
 	            		if(food.foodPosition[i][j] == 1) {
 	            			food.foodPosition[j][i] = 0;
 	            		}
@@ -47,12 +51,12 @@ public class Collisions {
     		for(int i = 0; i < widthNum; i++) {
 	            for(int j = 0; j < heightNum; j++) {
 
-	            	if ((proximity(snake2.getSnakeX(0), j * PIXELSIZE, 11))
-	                        && (proximity(snake2.getSnakeY(0), i * PIXELSIZE, 11))) {
+	            	if ((proximity(snake2.getSnakeX(0), j * PIXELSIZE, distance + 1))
+	                        && (proximity(snake2.getSnakeY(0), i * PIXELSIZE, distance + 1))) {
 	            		System.out.println("i: " + (i) * PIXELSIZE + "  SNAKE Y:" + snake2.getSnakeY(0));
 	            		System.out.println("j: " + (j) * PIXELSIZE + "  SNAKE X:" + snake2.getSnakeX(0));
 	            		 // Add score
-	                    snake2.setScore(snake2.getScore() + 1);
+	                    snake2.setScore(snake2.getScore() + fvScore);
 	            		if(food.foodPosition[i][j] == 1) {
 	            			food.foodPosition[j][i] = 0;
 	            		}
@@ -65,25 +69,25 @@ public class Collisions {
     // if our snake is in the close proximity of the food..
     void checkFoodCollisions() {
 
-        if ((proximity(snake1.getSnakeX(0), food.getFoodX(), 10))
-                && (proximity(snake1.getSnakeY(0), food.getFoodY(), 10))) {
+        if ((proximity(snake1.getSnakeX(0), food.getFoodX(), distance))
+                && (proximity(snake1.getSnakeY(0), food.getFoodY(), distance))) {
 
             System.out.println("intersection");
             // Add a 'joint' to our snake
             snake1.setJoints(snake1.getJoints() + 1);
             // Add score
-            snake1.setScore(snake1.getScore() + 100);
+            snake1.setScore(snake1.getScore() + nmScore);
             // Create new food
             food.createFood();
         }
-        if ((proximity(snake2.getSnakeX(0), food.getFoodX(), 10))
-                && (proximity(snake2.getSnakeY(0), food.getFoodY(), 10))) {
+        if ((proximity(snake2.getSnakeX(0), food.getFoodX(), distance))
+                && (proximity(snake2.getSnakeY(0), food.getFoodY(), distance))) {
 
             System.out.println("intersection");
             // Add a 'joint' to our snake
             snake2.setJoints(snake2.getJoints() + 1);
             // Add score
-            snake2.setScore(snake2.getScore() + 100);
+            snake2.setScore(snake2.getScore() + nmScore);
             // Create new food
             food.createFood();
         }
@@ -92,8 +96,8 @@ public class Collisions {
     // if our snake is in the close proximity of the poison..
     int checkPoisonCollisions() {
         for (int i = 0; i < poison.size(); i++) {
-            if ((proximity(snake1.getSnakeX(0), poison.get(i).getPoisonX(), 10))
-                    && (proximity(snake1.getSnakeY(0), poison.get(i).getPoisonY(), 10))) {
+            if ((proximity(snake1.getSnakeX(0), poison.get(i).getPoisonX(), distance))
+                    && (proximity(snake1.getSnakeY(0), poison.get(i).getPoisonY(), distance))) {
 
                 System.out.println("intersection");
                 // Add a 'joint' to our snake
@@ -104,8 +108,8 @@ public class Collisions {
                 }
                 // Create new food
                 relocation(poison, i);
-            } else if ((proximity(snake2.getSnakeX(0), poison.get(i).getPoisonX(), 10))
-                    && (proximity(snake2.getSnakeY(0), poison.get(i).getPoisonY(), 10))) {
+            } else if ((proximity(snake2.getSnakeX(0), poison.get(i).getPoisonX(), distance))
+                    && (proximity(snake2.getSnakeY(0), poison.get(i).getPoisonY(), distance))) {
 
                 System.out.println("intersection");
                 // Add a 'joint' to our snake
@@ -128,8 +132,8 @@ public class Collisions {
         // If the snake hits other snake's joints..
         for (int i = snake2.getJoints(); i > 0; i--) {
 
-        	if (proximity(snake1.getSnakeX(0), snake2.getSnakeX(i), 5)
-                    && proximity(snake1.getSnakeY(0), snake2.getSnakeY(i), 5)) {
+        	if (proximity(snake1.getSnakeX(0), snake2.getSnakeX(i), distance)
+                    && proximity(snake1.getSnakeY(0), snake2.getSnakeY(i), distance)) {
         			System.out.println("asfdjnaovjefakjfsanvkfj");
             		sndPlayerWin = true;
             		snake2.setScore(snake2.getScore() + snake1.getScore());
@@ -139,8 +143,8 @@ public class Collisions {
 
         for (int i = snake1.getJoints(); i > 0; i--) {
 
-            if (proximity(snake2.getSnakeX(0), snake1.getSnakeX(i), 5)
-                    && proximity(snake2.getSnakeY(0), snake1.getSnakeY(i), 5)) {
+            if (proximity(snake2.getSnakeX(0), snake1.getSnakeX(i), distance)
+                    && proximity(snake2.getSnakeY(0), snake1.getSnakeY(i), distance)) {
                 fstPlayerWin = true;
                 snake1.setScore(snake1.getScore() + snake2.getScore());
                 return 2; // then the game ends
